a6c139c2d7dee3ba8d2b0c63176904f94b433bcc
kkmiloo <camilo.reyes_granada@uao.edu.co>
2024-07-29 10:31:55 -0500

    add: Services to api

--- src/services/material.service.ts
+++ src/services/material.service.ts
@@ -1,4 +1,5 @@
 import { backendApi } from '../api/backend.api';
+import { CreateMaterialDto } from '../interfaces';
 import { MaterialI as MaterialResponse } from '../interfaces/material.interface';
 
 export class MaterialService {
@@ -10,6 +11,38 @@ export class MaterialService {
     } catch (error) {
       console.log(error);
       throw new Error('Error al obtener los materiales');
+    }
+  };
+
+  static createMaterial = async (
+    material: CreateMaterialDto
+  ): Promise<MaterialResponse> => {
+    try {
+      const { data } = await backendApi.post<MaterialResponse>(
+        '/material',
+        material
+      );
+
+      return data;
+    } catch (error) {
+      console.log(error);
+      throw new Error('Error al crear el material');
+    }
+  };
+
+  static updateMaterial = async (
+    material: Partial<MaterialResponse>
+  ): Promise<MaterialResponse> => {
+    const { id, ...data } = material;
+
+    try {
+      const { data: updatedMaterial } =
+        await backendApi.patch<MaterialResponse>(`/material/${id}`, data);
+
+      return updatedMaterial;
+    } catch (error) {
+      console.log(error);
+      throw new Error('Error al actualizar el material');
     }
   };
 }
--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -6,6 +6,7 @@ export class ProductService {
     try {
       const { data } = await backendApi.get<ProductI[]>('/product');
 
+      console.log({ get: data });
       return data;
     } catch (error) {
       console.log(error);
@@ -16,8 +17,27 @@ export class ProductService {
   static createProduct = async (product: CreateProductDto) => {
     try {
       const { data } = await backendApi.post<ProductI>('/product', product);
+      console.log({ create: data });
 
       return data;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  };
+
+  static updateProduct = async (product: Partial<ProductI>) => {
+    const { id, ...data } = product;
+
+    console.log({ id, data });
+
+    try {
+      const { data: updatedProduct } = await backendApi.patch<ProductI>(
+        `/product/${id}`,
+        data
+      );
+
+      return updatedProduct;
     } catch (error) {
       console.log(error);
       throw error;
